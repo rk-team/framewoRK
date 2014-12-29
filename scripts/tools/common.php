a37aab298c152dfd23d83cc33527ba45d27c7572
rophle <rophle@lpdc.eu>
2014-12-29 15:14:23 +0100

    createModule script

--- scripts/tools/common.php
+++ scripts/tools/common.php
@@ -13,11 +13,28 @@ class common {
 	public static $PRINT_RED = "\033[01;31m";
 
 	
-	public static function checkLauncher ($argv) {
+	public static function checkLauncher () {
 		
 		if (php_sapi_name() != "cli") {
 			echo 'This script must be launched in console mode !' . "\n";
 			exit(1);
 		}
+	}
+	
+	
+	
+	public static function useTpl($tplPath, $vars) {
+		$tpl = file_get_contents($tplPath);
+		foreach($vars as $key => $value) {
+			
+			$tpl = str_replace('@' . $key . '@',  $value, $tpl);
+		}
+		
+		return $tpl;
+	}
+	
+	public static function createFileFromTpl($tplPath, $tplVars, $destination) {
+		$data = self::useTpl($tplPath, $tplVars);
+		\rk\helper\fileSystem::file_put_contents($destination, $data);
 	}
 }
